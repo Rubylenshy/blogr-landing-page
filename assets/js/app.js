c4d251e09503623b1b772cc5a7441b8748cc4637
Rubylenshy <reztomoloju@gmail.com>
2022-10-04 23:29:17 +0100

    desktop view optimixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
 
 const hamburger = document.querySelector('.hamburger')
 const mobileNav = document.querySelector('.mobile-nav')
+const desktopNav = document.querySelector('.navbar-desktop')
 
-const arrow = mobileNav.querySelectorAll('.arrow')
-const arrowImg = mobileNav.querySelectorAll('.arrow-img')
-const dropdownList = mobileNav.querySelectorAll('.nav-li')
+const mobileArrow = mobileNav.querySelectorAll('.arrow')
+const mobileArrowImg = mobileNav.querySelectorAll('.arrow-img')
+const mobileDropDownList = mobileNav.querySelectorAll('.nav-li')
+
+const desktopArrow = desktopNav.querySelectorAll('.desktop-arrow')
+const desktopArrowImg = desktopNav.querySelectorAll('.desktop-arrow-img')
+const desktopDropDownList = desktopNav.querySelectorAll('.desktop-nav-li')
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('active')
@@ -13,11 +18,20 @@ hamburger.addEventListener('click', ()=>{
 
 // Dropdown
 
-for (let i = 0; i < arrow.length; i++) {
+for (let i = 0; i < mobileArrow.length; i++) {
     
-    arrow[i].addEventListener('click', ()=>{
-        arrowImg[i].classList.toggle('flipped');
-        dropdownList[i].classList.toggle('show');
+    mobileArrow[i].addEventListener('click', ()=>{
+        mobileArrowImg[i].classList.toggle('flipped');
+        mobileDropDownList[i].classList.toggle('show');
+    })
+    
+}
+
+for (let i = 0; i < desktopArrow.length; i++) {
+    
+    desktopArrow[i].addEventListener('click', ()=>{
+        desktopArrowImg[i].classList.toggle('flipped');
+        desktopDropDownList[i].classList.toggle('show');
     })
     
 }
